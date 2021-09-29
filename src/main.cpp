ee68e5c32656a1b6bf83486902c255879770f28b
suzana <antolin.suzana@gmail.com>
2021-09-29 13:44:14 +0200

    modified:   src/game_kaladont.txt 	modified:   src/main.cpp 	modified:   src/main.exe 	new file:   src/word.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,15 @@
-#include <iostream>
+//#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <algorithm>
+#include "word.hpp"
 
 using namespace std;
-class Word
-{
-public:
-    string text;
-    bool isUsed;
-    vector<Word> words;
-};
+
 bool operator<(const Word &s1, const Word &s2)
 {
-    if (s1.words.size() < s2.words.size())
+    if (s1.num_of_connectable_words < s2.num_of_connectable_words)
         return true;
     else
         return false;
@@ -27,7 +22,7 @@ vector<string> words_to_write;
 vector<Word> get_all_words();
 vector<Word> get_acceptable_words(string lastTwoLetters);
 void fill_nested_list_with_words_lists(vector<Word> &aceptable_list);
-
+void set_is_used(string word);
 void write_word_to_kaladont_game(string w);
 string choose_next_word(string w);
 int main()
@@ -37,6 +32,7 @@ int main()
 
     auto starting_word = all_words[65];
     write_word_to_kaladont_game(starting_word.text);
+    set_is_used(starting_word.text);
 
     words_to_write.push_back(starting_word.text);
 
@@ -44,7 +40,7 @@ int main()
     {
         string next = choose_next_word(words_to_write[words_to_write.size() - 1]);
         write_word_to_kaladont_game(next);
-
+        set_is_used(next);
         words_to_write.push_back(next);
         counter++;
     }
@@ -61,6 +57,7 @@ string choose_next_word(string w)
 
     sort(filtered.begin(), filtered.end(), [](Word &w, Word &ww)
          { return ww < w; });
+    cout << filtered[0].num_of_connectable_words << endl;
 
     return filtered[0].text;
 }
@@ -93,9 +90,8 @@ vector<Word> get_all_words()
 vector<Word> get_acceptable_words(string lastTwoLetters)
 {
     vector<Word> newlist;
-    newlist.clear();
     copy_if(all_words.begin(), all_words.end(), back_inserter(newlist), [lastTwoLetters](Word match)
-            { return (match.text.substr(0, 2).compare(lastTwoLetters) == 0) && match.isUsed == false && match.text.length() > 2 && match.text.substr(match.text.length() - 2, 2).compare(lastTwoLetters) != 0; });
+            { return (match.text.substr(0, 2).compare(lastTwoLetters) == 0) && match.isUsed == false && match.text.length() > 2; });
 
     return newlist;
 }
@@ -107,9 +103,15 @@ void fill_nested_list_with_words_lists(vector<Word> &aceptable_list)
     for (Word &word : aceptable_list)
     {
         vector<Word> newlis = get_acceptable_words(word.text.substr(word.text.length() - 2, 2));
-        for (auto w : newlis)
-        {
-            word.words.push_back(w);
-        }
+        word.num_of_connectable_words = newlis.size();
+    }
+}
+void set_is_used(string word)
+{
+    for (Word &w : all_words)
+    {
+        auto found = find_if(all_words.begin(), all_words.end(), [&, word](Word ww)
+                             { return ww.text.compare(word) == 0; });
+        found->isUsed = true;
     }
 }
---
+++ src/word.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+class Word
+{
+public:
+    std::string text;
+    bool isUsed;
+    int num_of_connectable_words;
+};
