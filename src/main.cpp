9e5f0d18584b5c81c3dc32dd8bb1807396e2f6ab
suzana <antolin.suzana@gmail.com>
2021-09-30 16:42:26 +0200

    MADE IT

--- src/main.cpp
+++ src/main.cpp
@@ -10,26 +10,16 @@ using namespace std;
 ofstream kaladont_file("game_kaladont.txt");
 vector<Word> all_words;
 vector<string> words_to_write;
-
+const string KA = "ka";
 vector<vector<Word>> jagged_words;
-void read_jagged_vector()
-{
-    for (size_t row = 0; row < jagged_words.size(); ++row)
-    {
-        for (size_t col = 0; col < jagged_words[row].size(); ++col)
-        {
-            cout << jagged_words[row][col].letter_group << endl;
-        }
-       
-    }
-}
+
 int main()
 {
     int counter = 1;
     //all_words = get_all_words_from_file();
     make_vector_of_vectors();
-    read_jagged_vector();
-    auto starting_word = all_words[66];
+
+    auto starting_word = jagged_words[1][2];
     write_word_to_kaladont_game(starting_word.text);
     set_is_used(starting_word.text);
 
@@ -43,7 +33,7 @@ int main()
         words_to_write.push_back(next);
         counter++;
     }
-
+    kaladont_file.close();
     return 0;
 }
 void make_vector_of_vectors()
@@ -76,7 +66,6 @@ void make_vector_of_vectors()
             }
             if (temp.size() > 0)
             {
-              
                 jagged_words.push_back(temp);
             }
         }
@@ -92,15 +81,13 @@ string choose_next_word(string w)
 
     sort(filtered.begin(), filtered.end(), [](Word &w, Word &ww)
          { return ww < w; });
-    cout << filtered[0].num_of_connectable_words << endl;
+    // cout << filtered[0].num_of_connectable_words << endl;
 
     return filtered[0].text;
 }
 void write_word_to_kaladont_game(string w)
 {
-    cout << w << endl;
     kaladont_file << w << endl;
-    //kaladont_file.close();
 }
 vector<Word> get_all_words_from_file()
 {
@@ -128,11 +115,26 @@ vector<Word> get_all_words_from_file()
     }
     return all_words;
 }
+
 vector<Word> get_acceptable_words(string lastTwoLetters)
 {
     vector<Word> newlist;
-    copy_if(all_words.begin(), all_words.end(), back_inserter(newlist), [lastTwoLetters](Word match)
-            { return (match.text.substr(0, 2).compare(lastTwoLetters) == 0) && match.isUsed == false && match.text.length() > 2; });
+    // copy_if(all_words.begin(), all_words.end(), back_inserter(newlist), [lastTwoLetters](Word match)
+    //         { return (match.text.substr(0, 2).compare(lastTwoLetters) == 0) && match.isUsed == false && match.text.length() > 2; });
+    for (size_t row = 0; row < jagged_words.size(); ++row)
+    {
+        if (jagged_words[row][0].letter_group.compare(lastTwoLetters) == 0)
+        {
+            for (size_t col = 0; col < jagged_words[row].size(); ++col)
+            {
+                if (jagged_words[row][col].isUsed == false && jagged_words[row][col].last_two.compare(KA) != 0)
+                {
+                    newlist.push_back(jagged_words[row][col]);
+                    //  cout << jagged_words[row][col].text << " ";
+                }
+            }
+        }
+    }
 
     return newlist;
 }
@@ -146,15 +148,28 @@ void fill_nested_list_with_words_lists(vector<Word> &aceptable_list)
         vector<Word> newlis = get_acceptable_words(word.text.substr(word.text.length() - 2, 2));
         word.num_of_connectable_words = newlis.size();
     }
-    cout << "fill_nested_list_with_words_lists done" << endl;
+    // cout << "fill_nested_list_with_words_lists done" << endl;
 }
 void set_is_used(string word)
 {
-    for (Word &w : all_words)
+    // for (Word &w : all_words)
+    // {
+    //     auto found = find_if(all_words.begin(), all_words.end(), [&, word](Word ww)
+    //                          { return ww.text.compare(word) == 0; });
+    //     found->isUsed = true;
+    // }
+    for (size_t row = 0; row < jagged_words.size(); ++row)
     {
-        auto found = find_if(all_words.begin(), all_words.end(), [&, word](Word ww)
-                             { return ww.text.compare(word) == 0; });
-        found->isUsed = true;
+        if (jagged_words[row][0].letter_group.compare(word.substr(0, 2)) == 0)
+        {
+            for (size_t col = 0; col < jagged_words[row].size(); ++col)
+            {
+                if (jagged_words[row][col].text.compare(word) == 0)
+                {
+                    jagged_words[row][col].isUsed = true;
+                    // cout << "set_is_used done" << endl;
+                }
+            }
+        }
     }
-    cout << "set_is_used done" << endl;
 }
